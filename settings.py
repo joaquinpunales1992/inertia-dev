e52c7b279dbf1dde52218b59b0ad4f2d6fc69ef2
joaquinpunales1992 <joaquinpunales@gmail.com>
2025-03-17 15:38:19 -0500

    update

--- settings.py
+++ settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "inertia_dev.urls"
+ROOT_URLCONF = "urls"
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "inertia_dev.wsgi.application"
+WSGI_APPLICATION = "wsgi.application"
 
 
 # Database
