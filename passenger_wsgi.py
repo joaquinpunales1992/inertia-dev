5e9c656f445c7acd2f6d28323307489a6ee2822b
joaquinpunales1992 <joaquinpunales@gmail.com>
2025-03-17 16:43:05 -0400

    prod

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,1 +1,6 @@
-from wsgi import application
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+application = get_wsgi_application()
