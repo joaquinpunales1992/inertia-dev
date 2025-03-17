cac0c879dc27d134ce1dad0241b45d68b9fef8ea
joaquinpunales1992 <joaquinpunales@gmail.com>
2025-03-17 15:13:21 -0500

    structure

--- asgi.py
+++ asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for inertia_dev project.
+ASGI config for planmymeals project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inertia_dev.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 application = get_asgi_application()
